5fef0279b791a8b1e21f8633baf596fbcac595c1
ecoalimentos <alberto.palomeque68@unach.mx>
2024-03-06 13:21:18 -0600

    Initial Commit

---
+++ ecodetective/ecodetectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecodetectiveApp.swift
+//  ecodetective
+//
+//  Created by ADMIN UNACH on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ecodetectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
