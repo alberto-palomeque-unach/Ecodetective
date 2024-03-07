be3b72a36b26cf9be7aa3dc51b47144f490c6450
ecoalimentos <alberto.palomeque68@unach.mx>
2024-03-07 09:10:59 -0600

    fixed intentation in NoticiasHeadlinesView.swift

--- ecodetective/NoticiasHeadlinesView.swift
+++ ecodetective/NoticiasHeadlinesView.swift
@@ -8,49 +8,51 @@
 import SwiftUI
 
 public struct NoticiasHeadlinesView: View {
-
-  @StateObject private var viewModel = NoticiasHeadlinesViewModel()
-  @State private var errorMessage: String? = nil
-
-  public init() {}
-
-  public var body: some View {
-    NavigationView {
-      if let errorMessage = errorMessage {
-        Text(errorMessage)
-          .fontWeight(.bold)
-      } else {
-        List(viewModel.articles) { article in
-          NavigationLink(
-            destination: {
-              if let url = article.url {
-                SFSafariViewWrapper(url: url)
-              }
-            },
-            label: { NoticiasArticleView(article: article) }
-          )
-            .listRowSeparator(.hidden)
+    
+    @StateObject private var viewModel = NoticiasHeadlinesViewModel()
+    @State private var errorMessage: String? = nil
+    
+    public init() {}
+    
+    public var body: some View {
+        NavigationView {
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .fontWeight(.bold)
+            } else {
+                List(viewModel.articles) { article in
+                    NavigationLink(
+                        destination: {
+                            if let url = article.url {
+                                SFSafariViewWrapper(url: url)
+                            }
+                            
+                        },
+                        label: { NoticiasArticleView(article: article) }
+                    )
+                    .listRowSeparator(.hidden)
+                }
+                .task {
+                    getArticles()
+                }
+                .listStyle(.plain)
+                .navigationTitle("Noticias")
+                .navigationBarTitleDisplayMode(.inline)
+            }
         }
-        .task {
-          getArticles()
+        .refreshable {
+            getArticles()
         }
-        .listStyle(.plain)
-        .navigationTitle("Headlines")
-      }
     }
-    .refreshable {
-      getArticles()
+    
+    private func getArticles() {
+        errorMessage = nil
+        do {
+            try viewModel.getArticles()
+        } catch {
+            errorMessage = error.localizedDescription
+        }
     }
-  }
-
-  private func getArticles() {
-    errorMessage = nil
-    do {
-      try viewModel.getArticles()
-    } catch {
-      errorMessage = error.localizedDescription
-    }
-  }
 }
 
 #Preview {
