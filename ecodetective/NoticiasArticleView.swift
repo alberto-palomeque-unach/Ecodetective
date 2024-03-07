f50cc9f20d3084757d627103ef1c64b21200683e
ecoalimentos <alberto.palomeque68@unach.mx>
2024-03-07 08:44:30 -0600

    Edited NoticiasArticleView.swift, added source

--- ecodetective/NoticiasArticleView.swift
+++ ecodetective/NoticiasArticleView.swift
@@ -9,38 +9,45 @@ import SwiftUI
 import NewsAPI
 
 public struct NoticiasArticleView: View {
-  private let article: NewsArticle
-
-  public init(article: NewsArticle) {
-    self.article = article
-  }
-
-  public var body: some View {
-    HStack {
-      Text(article.title ?? "")
-        .fontWeight(.bold)
-      AsyncImage(url: article.urlToImage) { phase in
-        switch phase {
-        case .empty:
-          ProgressView()
-        case .success(let image):
-          image.resizable()
-            .aspectRatio(1.5, contentMode: .fit)
-            .frame(maxWidth: 150)
-        case .failure:
-          VStack {
-            Image(systemName: "square.slash")
-            Text("Image is not found")
-              .fontWeight(.light)
-              .foregroundColor(.secondary)
-          }
-          .frame(width: 150)
-        @unknown default:
-          EmptyView()
-        }
-      }
+    private let article: NewsArticle
+    
+    public init(article: NewsArticle) {
+        self.article = article
     }
-  }
+    
+    public var body: some View {
+        VStack {
+            HStack {
+                Text(article.source.name)
+                    .fontWeight(.light)
+                Spacer()
+            }
+            HStack {
+                Text(article.title ?? "")
+                    .fontWeight(.bold)
+                AsyncImage(url: article.urlToImage) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image.resizable()
+                            .aspectRatio(1.5, contentMode: .fit)
+                            .frame(maxWidth: 150)
+                    case .failure:
+                        VStack {
+                            Image(systemName: "square.slash")
+                            Text("Imagen no encontrada")
+                                .fontWeight(.light)
+                                .foregroundColor(.secondary)
+                        }
+                        .frame(width: 150)
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+            }
+        }
+    }
 }
 
 #Preview {
